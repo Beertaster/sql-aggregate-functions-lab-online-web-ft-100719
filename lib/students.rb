2b6a9637a683df762245e26ca7bdbe73b870fcc9
William Stevens <wstevens.hornet@gmail.com>
2019-11-11 04:22:36 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,11 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  SELECT MIN(pga) FROM students
+  SELECT MIN(gpa) FROM students;
 end
 
 def average_student_gpa
-  
+  SELECT AVG(gpa) FROM stuents;
 end
 
 def total_tardies_for_all_students
